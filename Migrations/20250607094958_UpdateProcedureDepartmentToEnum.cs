aed0b0e118f3585140ecec43e089d08c810769e3
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-06-07 12:51:17 +0300

    procedures done

---
+++ Migrations/20250607094958_UpdateProcedureDepartmentToEnum.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProcedureDepartmentToEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Department",
+                table: "Procedures",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Department",
+                table: "Procedures",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
--- Models/Procedure.cs
+++ Models/Procedure.cs
@@ -2,6 +2,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HospitalManagement.Models
 {
+    public enum Department
+    {
+        Cardiology,
+        Neurology,
+        Orthopedics,
+        Pediatrics,
+        Surgery,
+        [Display(Name = "Internal Medicine")]
+        InternalMedicine,
+        Emergency,
+        Radiology
+    }
+
     public class Procedure
     {
         public int Id { get; set; }
@@ -11,8 +24,7 @@ namespace HospitalManagement.Models
         public string Name { get; set; }
 
         [Required]
-        [StringLength(100)]
-        public string Department { get; set; }
+        public Department Department { get; set; }
 
         [Required]
         public int Duration { get; set; } // Duration in minutes
